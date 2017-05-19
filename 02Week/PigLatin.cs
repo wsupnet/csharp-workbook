ff4ce9e0a601cd65aa88ecc615c169b4134e6749
Wesley S <wesleys@Wesleys-MacBook-Pro.local>
2017-05-18 20:16:36 -0500

    Added Pig Latin

--- 02Week/PigLatin.cs
+++ 02Week/PigLatin.cs
@@ -1,17 +1,52 @@
 using System;
 
-public class Program
+namespace ConsoleApplication
 {
-    public static void Main()
+
+    public class PigLatin
     {
-        Console.WriteLine("What's the word?");
-        string word = Console.ReadLine();
-        Console.WriteLine(translateWord(word));
-    }
-    
-    public static string translateWord(string word)
-    {
-        // Your code here
-        return word;
+        public void Run()
+        {
+            Console.WriteLine("What's the word?");
+            string word = Console.ReadLine();
+            Console.WriteLine(translateWord(word));
+        }
+
+        public static string translateWord(string word)
+        {
+            // Your code here
+            word = word.ToLower();
+            int vowelIndex = word.IndexOf("a");
+
+            if ( (word.IndexOf("e") > -1) && (word.IndexOf("e") < vowelIndex) || (vowelIndex == -1) ) 
+            {
+                vowelIndex = word.IndexOf("e");
+            }
+
+            if ( (word.IndexOf("i") > -1 && word.IndexOf('i') < vowelIndex) || (vowelIndex == -1) ) 
+            {
+                vowelIndex = word.IndexOf('i'); 
+            }
+
+            if ( (word.IndexOf("o") > -1 && word.IndexOf('o') < vowelIndex) || (vowelIndex == -1) )  
+            {
+                vowelIndex = word.IndexOf('o'); 
+            }
+
+            if ( (word.IndexOf("u") > -1 && word.IndexOf('u') < vowelIndex) || (vowelIndex == -1) )  
+            {
+                vowelIndex = word.IndexOf('u'); 
+            }
+
+            string firstPart = word.Substring(0, vowelIndex);
+            string restWord = word.Substring(vowelIndex, word.Length - 1);
+
+            Console.WriteLine("vowelIndex: " + vowelIndex);
+            Console.WriteLine("firstPart: " + firstPart);
+            Console.WriteLine("restWord: " + restWord)
+            Console.WriteLine(vowelIndex);
+
+            return "Pig Latin Word: " + restWord + firstPart + "ay";
+        }
     }
 }
--- 02Week/TicTacToe.cs
+++ 02Week/TicTacToe.cs
@@ -1,72 +1,72 @@
-using System;
+// using System;
 
-public class Program
-{
-    public static string playerTurn = "X";
-    public static string[][] board = new string[][]
-    {
-        new string[] {" ", " ", " "},
-        new string[] {" ", " ", " "},
-        new string[] {" ", " ", " "}
-     };
+// public class Program
+// {
+//     public static string playerTurn = "X";
+//     public static string[][] board = new string[][]
+//     {
+//         new string[] {" ", " ", " "},
+//         new string[] {" ", " ", " "},
+//         new string[] {" ", " ", " "}
+//      };
 
-    public static void Main()
-    {
-        // Your code here
-        DrawBoard();
-        GetInput();
+//     public static void Main()
+//     {
+//         // Your code here
+//         DrawBoard();
+//         GetInput();
 
-    }
+//     }
 
-    public static void GetInput()
-    {
-        Console.WriteLine("Player " + playerTurn);
-        Console.WriteLine("Enter Row:");
-        int row = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("Enter Column:");
-        int column = Int32.Parse(Console.ReadLine());
-        // Your code here
-        return;
-    }
+//     public static void GetInput()
+//     {
+//         Console.WriteLine("Player " + playerTurn);
+//         Console.WriteLine("Enter Row:");
+//         int row = Int32.Parse(Console.ReadLine());
+//         Console.WriteLine("Enter Column:");
+//         int column = Int32.Parse(Console.ReadLine());
+//         // Your code here
+//         return;
+//     }
 
-    public static void PlaceMark(int row, int column)
-    {
-        // Your code here
-        return;
-    }
+//     public static void PlaceMark(int row, int column)
+//     {
+//         // Your code here
+//         return;
+//     }
 
-    public static bool CheckForWin()
-    {
-        // Your code here
-        return false;
-    }
+//     public static bool CheckForWin()
+//     {
+//         // Your code here
+//         return false;
+//     }
 
-    public static bool HorizontalWin()
-    {
-        // Your code here
-        return false;
-    }
+//     public static bool HorizontalWin()
+//     {
+//         // Your code here
+//         return false;
+//     }
 
-    public static bool VerticalWin()
-    {
-        // Your code here
-        return false;
-    }
+//     public static bool VerticalWin()
+//     {
+//         // Your code here
+//         return false;
+//     }
 
-    public static bool DiagonalWin()
-    {
-        // Your code here
-        return false;
-    }
+//     public static bool DiagonalWin()
+//     {
+//         // Your code here
+//         return false;
+//     }
 
-    public static void DrawBoard()
-    {
-        Console.WriteLine("  0 1 2");
-        Console.WriteLine("0 " + String.Join("|", board[0]));
-        Console.WriteLine("  -----");
-        Console.WriteLine("1 " + String.Join("|", board[1]));
-        Console.WriteLine("  -----");
-        Console.WriteLine("2 " + String.Join("|", board[2]));
-        return;
-    }
-}
+//     public static void DrawBoard()
+//     {
+//         Console.WriteLine("  0 1 2");
+//         Console.WriteLine("0 " + String.Join("|", board[0]));
+//         Console.WriteLine("  -----");
+//         Console.WriteLine("1 " + String.Join("|", board[1]));
+//         Console.WriteLine("  -----");
+//         Console.WriteLine("2 " + String.Join("|", board[2]));
+//         return;
+//     }
+// }
