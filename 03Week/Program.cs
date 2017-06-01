59b1e4b8354140177d1ed686bb065d94edb434e2
Wesley S <wesleys@Wesleys-MacBook-Pro.local>
2017-06-01 18:47:52 -0500

    Finished rock paper scissors

--- 03Week/Program.cs
+++ 03Week/Program.cs
@@ -8,69 +8,71 @@ namespace _03Week
         static void Main(string[] args)
         {
 
-            Land land = new Land();
-            Console.WriteLine("How many houses do you want?");
-            land.house = Convert.ToInt32(Console.ReadLine());
-            int countHouses = 1;
+            RockPaperScissors rockgame = new RockPaperScissors();
+            rockgame.Run();
 
-            Console.WriteLine("Do want a basketball court or a pool?");
-            land.activity = Console.ReadLine();
-            if (land.activity == "pool"){
-                Console.WriteLine("Your property will have a pool");
-            } else if (land.activity == "basketball court") {
-                Console.WriteLine("Your property will have a basketball court");
-            } else {
-                Console.WriteLine("I'm sorry, {0} isn't available", land.activity);
-            }
+            // Land land = new Land();
+            // Console.WriteLine("How many houses do you want?");
+            // land.house = Convert.ToInt32(Console.ReadLine());
+            // int countHouses = 1;
 
-            while (countHouses <= land.house)
-            {
+            // Console.WriteLine("Do want a basketball court or a pool?");
+            // land.activity = Console.ReadLine();
+            // if (land.activity == "pool"){
+            //     Console.WriteLine("Your property will have a pool");
+            // } else if (land.activity == "basketball court") {
+            //     Console.WriteLine("Your property will have a basketball court");
+            // } else {
+            //     Console.WriteLine("I'm sorry, {0} isn't available", land.activity);
+            // }
 
-                House house = new House();
+            // while (countHouses <= land.house)
+            // {
 
-                //Ask how many floors the house will have 
-                Console.WriteLine("How many floors do you want?");
-                house.floors = Convert.ToInt32(Console.ReadLine());
-                int countFloors = 1;
+            //     House house = new House();
 
-
-
-                while (countFloors <= house.floors)
-                {
-
-                    Console.WriteLine("How many rooms do you want on floor number {0}?", countFloors);
-                    house.rooms = Convert.ToInt32(Console.ReadLine());
-
-                    //We have to count how many rooms the house wiil have  
-                    int countRooms = 1;
-                    while (countRooms <= house.rooms)
-                    {
-
-                        Room room = new Room();
-                        Console.WriteLine("What color walls do you want your room to have?", countRooms);
-                        room.color = Console.ReadLine();
-
-                        string colorResponse = string.Format("Your room's walls will be {0}", room.color);
-                        Console.WriteLine(colorResponse);
-
-                        Console.WriteLine("What type of floor do you want your room to be?");
-                        room.floorType = Console.ReadLine();
-                        string floorTypeResponse = string.Format("Your floor type will be {0}", room.floorType);
-                        Console.WriteLine(floorTypeResponse);
-
-                        Console.WriteLine("How many windows do you want in your room?");
-                        room.window = Convert.ToInt32(Console.ReadLine());
-                        string windowResponse = string.Format("You room will have {0} windows", room.window);
-                        Console.WriteLine(windowResponse);
+            //     //Ask how many floors the house will have 
+            //     Console.WriteLine("How many floors do you want?");
+            //     house.floors = Convert.ToInt32(Console.ReadLine());
+            //     int countFloors = 1;
 
 
-                        countRooms++;
-                    }
-                    countFloors++;
 
-                }
-                countHouses++;
+            //     while (countFloors <= house.floors)
+            //     {
+
+            //         Console.WriteLine("How many rooms do you want on floor number {0}?", countFloors);
+            //         house.rooms = Convert.ToInt32(Console.ReadLine());
+
+            //         //We have to count how many rooms the house wiil have  
+            //         int countRooms = 1;
+            //         while (countRooms <= house.rooms)
+            //         {
+
+            //             Room room = new Room();
+            //             Console.WriteLine("What color walls do you want your room to have?", countRooms);
+            //             room.color = Console.ReadLine();
+
+            //             string colorResponse = string.Format("Your room's walls will be {0}", room.color);
+            //             Console.WriteLine(colorResponse);
+
+            //             Console.WriteLine("What type of floor do you want your room to be?");
+            //             room.floorType = Console.ReadLine();
+            //             string floorTypeResponse = string.Format("Your floor type will be {0}", room.floorType);
+            //             Console.WriteLine(floorTypeResponse);
+
+            //             Console.WriteLine("How many windows do you want in your room?");
+            //             room.window = Convert.ToInt32(Console.ReadLine());
+            //             string windowResponse = string.Format("You room will have {0} windows", room.window);
+            //             Console.WriteLine(windowResponse);
+
+
+            //             countRooms++;
+            //         }
+            //         countFloors++;
+
+            //     }
+            //     countHouses++;
             }
         }
     }
-}
--- 03Week/RockPaperScissors.cs
+++ 03Week/RockPaperScissors.cs
@@ -1,19 +1,64 @@
-// using System;
+using System;
 
-// public class Program
-// {
-// //     public static void Main()
-// //     {
-// //         Console.WriteLine("Enter hand 1:");
-// //         string hand1 = Console.ReadLine().ToLower();
-// //         Console.WriteLine("Enter hand 2:");
-// //         string hand2 = Console.ReadLine().ToLower();
-// //         Console.WriteLine(CompareHands(hand1, hand2));
-// //     }
-    
-// //     public static string CompareHands(string hand1, string hand2)
-// //     {
-// //         // Your code here
-// //         return hand1 + " " + hand2;
-// //     }
-// // }
+namespace _03Week
+{
+    public class RockPaperScissors
+    {
+        public void Run()
+        {
+            Console.WriteLine("Enter hand 1:");
+            string hand1 = Console.ReadLine().ToLower();
+            Console.WriteLine("Enter hand 2:");
+            string hand2 = Console.ReadLine().ToLower();
+            Console.WriteLine(CompareHands(hand1, hand2));
+        }
+
+        public static string CompareHands(string hand1, string hand2)
+        {
+            // Your code here
+            // return hand1 + " " + hand2;
+
+            if (hand1 == hand2)
+            {
+                return "It's a tie!";
+            }
+            if (hand1 == "scissors")
+            {
+                if (hand2 == "paper")
+                {
+                    return "Player 1 has won";
+                }
+                else
+                {
+                    return "Player 2 has won";
+                }
+            }
+            if (hand1 == "rock")
+            {
+                if (hand2 == "scissors")
+                {
+                    return "Player 1 has won";
+                }
+                else
+                {
+                    return "Player 2 has won";
+                }
+            }
+            if (hand1 == "paper")
+            {
+                if (hand2 == "rock")
+                {
+                    return "Player 1 has won";
+                }
+                else
+                {
+                    return "Player 2 has won";
+                }
+            }
+
+            return "Invalid hands";
+        }
+    }
+
+
+}
